02b68492f48f14eedf613d3fe34ab97c76576a59
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-11-12 12:36:04 +0700

    fix:deploy

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+const app = express();
+
 dotenv.config();
 
 const PORT = process.env.PORT;
-
-const app = express();
 
 app.use(cors());
 app.use(express.json());
